256a13167216e54f2801ae8fef31f7f95624f247
duhankosali <duhanksali@gmail.com>
2023-07-29 19:05:34 +0300

    Create Application with Text editor (Trumbowyg)

--- Github.AdvertisementApp.UI/Controllers/ApplicationController.cs
+++ Github.AdvertisementApp.UI/Controllers/ApplicationController.cs
@@ -1,4 +1,5 @@
 ﻿using Github.AdvertisementApp.Business.Interfaces;
+using Github.AdvertisementApp.Dtos;
 using Github.AdvertisementApp.UI.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,17 @@ namespace Github.AdvertisementApp.UI.Controllers
         {
             var response = await _advertisementService.GetAllAsync(); // admin tarafında olduğumuz için aktif-pasif hepsi geliyor.
             return this.ResponseView(response);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new AdvertisementCreateDto());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(AdvertisementCreateDto dto)
+        {
+            var response = await _advertisementService.CreateAsync(dto);    
+            return this.ResponseRedirectAction(response, "List");
         }
     }
 }
